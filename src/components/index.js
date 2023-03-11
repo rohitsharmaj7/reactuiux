16f5074176d39e13f58d5db504bb40030de6dc63
rohitsharmaj7 <rohitsharmaj7@gmail.com>
2023-03-11 12:48:52 +0530

    header completed

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// export { default as Article } from '../../helper/components/article/Article';
+// export { default as Brand } from '../../helper/components/brand/Brand';
+// export { default as CTA } from '../../helper/components/cta/CTA';
+// export { default as Feature } from '../../helper/components/feature/Feature';
+export { default as Navbar } from "./navbar/Navbar";
